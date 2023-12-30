e5b82d249c3c27dc2f81d6129fd7300c44cd5924
LizaMoh <143883888+LizaMoh@users.noreply.github.com>
2023-12-30 11:38:02 +0300

    pon

--- js/script.js
+++ js/script.js
@@ -146,3 +146,41 @@ function setClock(selector,endtime){
     }
 }
 setClock('.timer',timeEnd);
+
+//calc
+const result = document.querySelector('.calculating__result span');
+let sex,height,weight,age,ratio;
+function calcTotal(){
+    if(!sex || !height || !age || !ratio || !weight){
+        result.textContent = "0";
+        return;
+    }
+    if(sex ==="female"){
+        result.textContent = Math.round((447.6+(9.2*weight)+(3.1 * height)-(4.3*age))*ratio);
+    }else{
+        result.textContent = Math.round((88.6+(13.4*weight)+(4.8 * height)-(5.7*age))*ratio);
+    }
+}
+calcTotal();
+function getStaticInformation(parentSelector,activeClass){
+    const elements = document.querySelectorAll('${parentSelector} div');
+
+    elements.forEach(elem =>{
+        elem.addEventListener('click',(e)=>{
+            if(e.target.getAttribute('data-ratio')){
+                ratio = e.target.getAttribute('data-ratio');
+            }
+            else{
+                sex = e.target.getAttribute('id');
+            }
+
+            elements.forEach(elem=>{
+                elem.classList.remove(activeClass);
+            })
+            e.target.classList.add(activeClass);
+            calcTotal();
+        })
+    })
+}
+getStaticInformation('#gender','calculating__choose-item_active');
+getStaticInformation('.calculating__choose_big','calculating__choose-item_active');
